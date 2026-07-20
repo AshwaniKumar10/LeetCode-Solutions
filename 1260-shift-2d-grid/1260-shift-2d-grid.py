0b64d59f8464f3d1f6125398e1a8664aad5826ff
Ashwani Kumar <ashwanikr1909@gmail.com>
2026-07-20 08:42:21 +0530

    Time: 7 ms (61.05%), Space: 12.7 MB (15.79%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        m = len(grid)
+        n = len(grid[0])
+
+        arr = []
+
+        
+        for row in grid:
+            arr.extend(row)
+
+        total = m * n
+        k %= total
+
+        
+        arr = arr[-k:] + arr[:-k]
+
+        
+        ans = []
+        idx = 0
+
+        for i in range(m):
+            row = []
+            for j in range(n):
+                row.append(arr[idx])
+                idx += 1
+            ans.append(row)
+
+        return ans
